60dc94b3c83dcea68e896b035ab6d3f2b6ee92e1
Joe Pelz <joe.pelz@gmail.com>
2023-03-09 20:30:18 -0800

    Adding Redis connection info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
   def index
-    render json: {status: "success"}, status: 200
+    client = ::RedisInstance.instance
+    old_value = client.get("test")
+    new_value = Time.current.as_json
+    client.set('test', new_value, ex: 10)
+    response_data = {
+        status: 'success',
+        old_value: old_value,
+        new_value: new_value
+    }
+    render json: response_data, status: 200
   end
 end
---
+++ app/lib/redis_instance.rb
@@ -0,0 +1,17 @@
+class RedisInstance
+  # TODO: put into an environment variable or Rails.config
+  # TODO: secure redis with credentials
+  # TODO: use connection_pool gem for multiple threads to manage redis connections
+  REDIS_URL = 'redis://localhost:6379'
+  REDIS_DATABASE = 0
+  OPTIONS = {
+      url: REDIS_URL,
+      # username: admin
+      # password: P4ssw0rd!
+      db: REDIS_DATABASE,
+  }.freeze
+
+  def self.instance
+    Redis.new(OPTIONS)
+  end
+end
